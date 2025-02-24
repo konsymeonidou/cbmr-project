8f4554fdd17f0d65df12d5dcb3f5fe7f3005d034
konsymeonidou <symeonidou.kon@gmail.com>
2025-02-24 11:37:15 +0200

    wip

--- app/routes/candidate.py
+++ app/routes/candidate.py
@@ -1,25 +1,25 @@
-from fastapi import APIRouter, Body, Request, status
-from typing import List
-from models import Candidate
-import rules as Candidates
+# from fastapi import APIRouter, Body, Request, status
+# from typing import List
+# from models.candidate import Candidate
+# import rules.candidate as Candidates
 
 
-router = APIRouter(prefix="/Candidate",
-    tags=["Candidate"])
+# router = APIRouter(prefix="/Candidate",
+#     tags=["Candidate"])
 
-@router.post("/", response_description="Create a new Candidate", status_code=status.HTTP_201_CREATED, response_model=Candidate)
-def create_Candidate(request: Request, Candidate: Candidate = Body(...)):  
-    return Candidates.create_Candidate(request,Candidate)
+# @router.post("/", response_description="Create a new Candidate", status_code=status.HTTP_201_CREATED, response_model=Candidate)
+# def create_Candidate(request: Request, Candidate: Candidate = Body(...)):  
+#     return Candidates.create_Candidate(request,Candidate)
 
-@router.get("/", response_description="List Candidates", response_model=List[Candidate])
-def list_Candidates(request: Request):
-    return Candidates.list_Candidates(request, 100)
+# @router.get("/", response_description="List Candidates", response_model=List[Candidate])
+# def list_Candidates(request: Request):
+#     return Candidates.list_Candidates(request, 100)
 
-@router.get("/{id}", response_description="Get a single Candidate by id", response_model=Candidate)
-def find_Candidate(request: Request, id: str):    
-    return Candidates.find_Candidate(request, id)
+# @router.get("/{id}", response_description="Get a single Candidate by id", response_model=Candidate)
+# def find_Candidate(request: Request, id: str):    
+#     return Candidates.find_Candidate(request, id)
 
 
-@router.delete("/{id}", response_description="Delete a Candidate")
-def delete_Candidate(request: Request, id:str):
-    return Candidates.delete_Candidate(request, id)
+# @router.delete("/{id}", response_description="Delete a Candidate")
+# def delete_Candidate(request: Request, id:str):
+#     return Candidates.delete_Candidate(request, id)
--- app/routes/metrics.py
+++ app/routes/metrics.py
@@ -1,32 +1,25 @@
 from fastapi import APIRouter, Request, status
-from typing import List
-from models import HealthMetrics
-from rules import Metrics
+from models.metrics import HealthMetrics
+from rules.metrics import create_HealthMetrics
 
-
-
-router = APIRouter(prefix="/metrics",
-    tags=["Metrics"])
+router = APIRouter(prefix="/metrics", tags=["metrics"])
 
 @router.post("/", response_description="Create health metrics", status_code=status.HTTP_201_CREATED, response_model=HealthMetrics)
 def create_metrics(request: Request, metrics: HealthMetrics):
-    return Metrics.create_metrics(request, metrics)
+    return create_HealthMetrics(request, metrics)
 
+# @router.put("/{health_id}", response_description="Update health metrics", response_model=HealthMetrics)
+# def update_metrics(request: Request, id: str, metrics: HealthMetrics):
+#     return metrics.update_metrics(request, id, metrics)
 
-@router.put("/{health_id}", response_description="Update health metrics", response_model=HealthMetrics)
-def update_metrics(request: Request, id: str, metrics: HealthMetrics):
-    return Metrics.update_metrics(request, id, metrics)
+# @router.get("/", response_description="List all Metrics", response_model=List[HealthMetrics])
+# def list_Metrics(request: Request):
+#     return metrics.list_Metrics(request, 100)
 
-@router.get("/", response_description="List all Metrics", response_model=List[HealthMetrics])
-def list_Metrics(request: Request):
-    return Metrics.list_Metrics(request, 100)
+# @router.get("/{candidate_id}/", response_description="List Metrics by candidate id", response_model=HealthMetrics)
+# def list_Metrics_by_id(request: Request, candidate_id: str):
+#     return metrics.list_Metrics_by_id(request, candidate_id)
 
-
-@router.get("/{candidate_id}/", response_description="List Metrics by candidate id", response_model=HealthMetrics)
-def list_Metrics_by_id(request: Request, candidate_id: str):
-    return Metrics.list_Metrics_by_id(request, candidate_id)
-
-
-@router.delete("/{health_id}", response_description="Delete health metrics")
-def delete_metrics(request: Request, id: str):
-    return Metrics.delete_metrics(request, id)
+# @router.delete("/{health_id}", response_description="Delete health metrics")
+# def delete_metrics(request: Request, id: str):
+#     return metrics.delete_metrics(request, id)
